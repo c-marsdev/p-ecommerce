36e0af035106d98aac67d0285f7c5f203ec8e26b
Mardes <puchetamarilin@gmail.com>
2025-10-18 23:04:12 -0300

    Se modificó  el componente Carrito

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -1,22 +1,35 @@
 const Carrito = ({ productosEnCarrito, productosEliminados }) => {
+  const total = productosEnCarrito.reduce(
+    (sum, item) => sum + Number(item.price),
+    0
+  );
+
   return (
     <div>
-      <h2>Carrito</h2>
-      {productosEnCarrito.map((producto, indice) => (
-        <div key={indice}>
-          <img
-            src={producto.image}
-            alt={producto.title}
-            height={80}
-            width={80}
-          />
-          <p>
-            {" "}
-            {producto.title} : {producto.price}${" "}
-          </p>
-          <button onClick={() => productosEliminados(indice)}>Eliminar</button>
-        </div>
-      ))}
+      <h2>Carrito de Compras</h2>
+      {productosEnCarrito.length === 0 ? (
+        <p>Carrito vacío</p>
+      ) : (
+        <>
+          {productosEnCarrito.map((producto, indice) => (
+            <div key={indice}>
+              <img
+                src={producto.image}
+                alt={producto.title}
+                height={80}
+                width={80}
+              />
+              <p>
+                {producto.title} : {producto.price}$
+              </p>
+              <button onClick={() => productosEliminados(indice)}>
+                Eliminar
+              </button>
+            </div>
+          ))}
+          <p>Total: ${Number(total).toFixed(3)}</p>
+        </>
+      )}
     </div>
   );
 };
